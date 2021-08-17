c908c9c735a99cb075eb56b1c009990b169884b1
smalaca <ab13.krakow@gmail.com>
2021-08-17 11:57:08 +0200

    add reference in tests why package structure is CQRS

--- greenfield/src/test/java/com/smalaca/architecture/PackageStructureTest.java
+++ greenfield/src/test/java/com/smalaca/architecture/PackageStructureTest.java
@@ -1,8 +1,10 @@
 package com.smalaca.architecture;
 
 import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+@Tag("2021-08-13-code-structure.md")
 public class PackageStructureTest {
     private static final String JAVA = "java..";
     private static final String REST = "..rest..";
@@ -16,6 +18,7 @@ public class PackageStructureTest {
                 .resideInAPackage(REST)
                 .should().onlyAccessClassesThat()
                 .resideInAnyPackage(JAVA, REST, COMMAND, QUERY)
+                .because("2021-08-13-code-structure.md")
                 .check(GreenFieldAppClasses.get());
     }
 
@@ -24,6 +27,7 @@ public class PackageStructureTest {
         ArchRuleDefinition.classes().that()
                 .resideInAPackage(COMMAND)
                 .should().onlyAccessClassesThat().resideInAnyPackage(COMMAND, JAVA)
+                .because("2021-08-13-code-structure.md")
                 .check(GreenFieldAppClasses.get());
     }
 
@@ -32,6 +36,7 @@ public class PackageStructureTest {
         ArchRuleDefinition.classes().that()
                 .resideInAPackage(QUERY)
                 .should().onlyAccessClassesThat().resideInAnyPackage(QUERY, JAVA, PERSISTANCY)
+                .because("2021-08-13-code-structure.md")
                 .check(GreenFieldAppClasses.get());
     }
 
@@ -40,6 +45,7 @@ public class PackageStructureTest {
         ArchRuleDefinition.classes().that()
                 .resideInAPackage(PERSISTANCY)
                 .should().onlyAccessClassesThat().resideInAnyPackage(PERSISTANCY, JAVA, COMMAND)
+                .because("2021-08-13-code-structure.md")
                 .check(GreenFieldAppClasses.get());
     }
 }
